bb0551673308255ec764f03f05937669874b160e
Gosia <gosia.koduje@gmail.com>
2020-10-26 20:37:26 +0100

    ma przejsc

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,6 +5,7 @@ public class MainTest {
     @org.junit.Test
     public void adder() {
         Main maintest = new Main();
-        assertEquals(10, maintest.adder(5,5));
+        assertEquals(25, maintest.adder(5,5));
+
     }
 }
